122a960ae36ad231b2d8e4b3f21c70c4cc196329
letzfets <arnold.knott@gmail.com>
2023-10-13 16:35:21 +0200

    feat: adds database setup

---
+++ backend/database.py
@@ -0,0 +1,36 @@
+"""Database schema for tables"""
+
+import databases
+import sqlalchemy
+
+# Python runs modules on import
+from config import config
+
+metadata = sqlalchemy.MetaData()
+
+post_table = sqlalchemy.Table(
+    "posts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+)
+
+comment_table = sqlalchemy.Table(
+    "comments",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False),
+)
+
+engine = sqlalchemy.create_engine(
+    config.DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    # echo=True for debugging
+)
+
+metadata.create_all(engine)
+
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
+)
