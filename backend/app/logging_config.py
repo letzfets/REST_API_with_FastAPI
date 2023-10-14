15c23fe7fe26503d341cab249eded7ac270668c8
letzfets <arnold.knott@gmail.com>
2023-10-14 15:31:27 +0200

    feat: saves logs in JSON format to file

--- backend/app/logging_config.py
+++ backend/app/logging_config.py
@@ -25,7 +25,7 @@ def configure_logging() -> None:
                     "format": "(%(correlation_id)s) %(name)s: %(lineno)d in %(module)s: %(message)s",
                 },
                 "file": {
-                    "class": "logging.Formatter",
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
                     "datefmt": "%Y-%m-%d %H:%M:%S",
                     "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | [%(correlation_id)s] %(name)s:%(lineno)d - %(message)s",
                 },
