4a04512807e2d71d7e747ad6e80414e203390889
letzfets <arnold.knott@gmail.com>
2023-10-14 21:06:43 +0200

    test: adds tests for password hashing

--- backend/app/tests/test_security.py
+++ backend/app/tests/test_security.py
@@ -2,6 +2,13 @@ import pytest
 from app import security
 
 
+def test_password_hashing():
+    """Test that we can hash a password."""
+    password = "1234"
+    hashed_password = security.get_password_hash(password)
+    assert security.verify_password(password, hashed_password)
+
+
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
     """Test that we can get a user from the database."""
