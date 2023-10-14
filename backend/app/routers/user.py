334ebccc1cd6428e5cff8466bb2351edab88d841
letzfets <arnold.knott@gmail.com>
2023-10-14 21:06:06 +0200

    feat: adds hashing to the password

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -2,7 +2,7 @@ import logging
 
 from app.database import database, user_table
 from app.models.user import UserIn
-from app.security import get_user
+from app.security import get_password_hash, get_user
 from fastapi import APIRouter, HTTPException, status
 
 logger = logging.getLogger(__name__)
@@ -17,8 +17,8 @@ async def register(user: UserIn):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="A user with that email already exists",
         )
-    # TODO: hash the password!!!
-    query = user_table.insert().values(email=user.email, password=user.password)
+    hashed_password = get_password_hash(user.password)
+    query = user_table.insert().values(email=user.email, password=hashed_password)
     logger.debug(query)
     await database.execute(query)
     return {"detail": "User created"}
