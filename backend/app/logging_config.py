60dcf4399cd19df96edb6d78ff12d6dcdba3ce3c
letzfets <arnold.knott@gmail.com>
2023-10-14 13:50:52 +0200

    feat: adds logging to files

--- backend/app/logging_config.py
+++ backend/app/logging_config.py
@@ -14,22 +14,36 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%d %H:%M:%S",
                     "format": "%(name)s: %(lineno)d in %(module)s: %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "app.log",
+                    "maxBytes": 1024 * 1024,
+                    "backupCount": 2,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
                 "uvicorn": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "INFO",
                 },
                 "databases": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "WARNING",
                 },
                 "aiosqlite": {
