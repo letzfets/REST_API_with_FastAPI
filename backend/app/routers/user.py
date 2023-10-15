19889fc4c3c426b397d28626b82bd03f652bc1be
letzfets <arnold.knott@gmail.com>
2023-10-15 20:55:28 +0200

    feat: adds user confirmation endpoint and tests for it

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -5,17 +5,19 @@ from app.models.user import UserIn
 from app.security import (
     authenticate_user,
     create_access_token,
+    create_confirmation_token,
     get_password_hash,
+    get_subject_for_token_type,
     get_user,
 )
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Request, status
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
 @router.post("/register", status_code=201)
-async def register(user: UserIn):
+async def register(user: UserIn, request: Request):
     """This is the register path of the API"""
     if await get_user(user.email):
         raise HTTPException(
@@ -26,7 +28,12 @@ async def register(user: UserIn):
     query = user_table.insert().values(email=user.email, password=hashed_password)
     logger.debug(query)
     await database.execute(query)
-    return {"detail": "User created"}
+    return {
+        "detail": "User created. PLease confirm your email.",
+        "confirmation_url": request.url_for(
+            "confirm_email", token=create_confirmation_token(user.email)
+        ),
+    }
 
 
 @router.post("/token")
@@ -35,3 +42,16 @@ async def login(user: UserIn):
     user = await authenticate_user(user.email, user.password)
     access_token = create_access_token(user.email)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/confirm/{token}")
+async def confirm_email(token: str):
+    """This is the confirm path of the API"""
+    email = get_subject_for_token_type(token, "confirmation")
+    query = (
+        user_table.update().where(user_table.c.email == email).values(confirmed=True)
+    )
+    logger.debug(query)
+    await database.execute(query)
+
+    return {"detail": "User confirmed"}
--- backend/app/tests/routers/test_user.py
+++ backend/app/tests/routers/test_user.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi import Request
 from httpx import AsyncClient
 
 
@@ -28,6 +29,36 @@ async def test_register_user_already_exists(
     )
     assert response.status_code == 400
     assert "already exists" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_confirm_user(async_client: AsyncClient, mocker):
+    """Test that we can confirm a user."""
+    spy = mocker.spy(Request, "url_for")
+    await register_user(async_client, "test@example.com", "1234")
+    confirmation_url = str(spy.spy_return)
+    response = await async_client.get(confirmation_url)
+    assert response.status_code == 200
+    assert "User confirmed" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_confirm_user_invalid_token(async_client: AsyncClient):
+    """Test that we cannot confirm a user with an invalid token."""
+    response = await async_client.get("/confirm/invalidtoken")
+    assert response.status_code == 401
+
+
+@pytest.mark.anyio
+async def test_confirm_user_expired_token(async_client: AsyncClient, mocker):
+    """Test that we can confirm a user."""
+    mocker.patch("app.security.confirm_token_expire_minutes", return_value=-1)
+    spy = mocker.spy(Request, "url_for")
+    await register_user(async_client, "test@example.com", "1234")
+    confirmation_url = str(spy.spy_return)
+    response = await async_client.get(confirmation_url)
+    assert response.status_code == 401
+    assert "Token has expired" in response.json()["detail"]
 
 
 @pytest.mark.anyio
